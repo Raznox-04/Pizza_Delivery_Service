7c68e334d48b904a8a525c75a1991cf46b1bc1de
sadra khamesi <sadrakhamesi.webdev@gmail.com>
2025-12-20 11:20:42 +0330

    Create Dependency of database

--- app/database/database.py
+++ app/database/database.py
@@ -1,6 +1,14 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from session import Session
 import os
 load_dotenv()
 POSTGRES_URI = os.getenv("POSTGRES_URI")
 engine = create_engine(POSTGRES_URI,echo=True)
+
+def get_database():
+    db =Session()
+    try:
+        yield db
+    finally:
+        db.close()
