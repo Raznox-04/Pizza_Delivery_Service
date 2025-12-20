304ce10107d031d81a3d9998511663d2a5bcdeef
sadra khamesi <sadrakhamesi.webdev@gmail.com>
2025-12-20 10:28:37 +0330

    commit Users and Orders

--- app/models/users.py
+++ app/models/users.py
@@ -1,9 +1,7 @@
-from app.database.base import Base
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey,Text
+from base import Base
+from sqlalchemy import Column, Integer, String, Boolean,Text
 from sqlalchemy.orm import relationship
-from .orders import Orders
-from uuid import UUID
-class User(Base):
+class Users(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     username = Column(String(length=25,), unique=True)
@@ -11,10 +9,8 @@ class User(Base):
     password = Column(Text, nullable=True)
     is_staff = Column(Boolean, default=False)
     is_active = Column(Boolean, default=False)
-    orders = relationship(Orders, back_populates="users")
-
+    orders = relationship("Orders", back_populates="users")
 
     def __repr__(self):
         return f"<User(id={self.id}, username={self .username}, email={self.email})>"
-
 
--- app/routers/orders.py
+++ app/routers/orders.py
@@ -1,5 +1,29 @@
-from fastapi import APIRouter
-order_router = APIRouter(prefix="/api",tags=["orders"])
-@order_router.get("/orders")
-async def root():
-    return {"message": "its working"}
+from base import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils.types import ChoiceType
+
+class Orders(Base):
+    ORDER_STATUS = (
+    ("PENDING", "pending"),
+    ("IN-TRANSACTION", "in_transaction"),
+    ("DELIVERED", "delivered"),
+    )
+
+    PIZZA_SIZES = (
+    ("SMALL","small"),
+    ("MEDIUM","medium"),
+    ("LARGE","large"),
+    ("EXTERA-LARGE","extera-large"),
+    )
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True)
+    quantity = Column(Integer,nullable=False)
+    order_status = Column(ChoiceType(choices=ORDER_STATUS),default=ORDER_STATUS[0][0])
+    pizza_size = Column(ChoiceType(choices=PIZZA_SIZES),default=PIZZA_SIZES[0][0])
+    flavour = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("Users",back_populates="orders")
+
+    def __repr__(self):
+        return f"Order Details(id={self.id}, quantity={self.quantity}, order_status={self.order_status})"
