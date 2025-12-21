05767286f442363fc592f0a70c90db1aceba4b6c
sadra khamesi <sadrakhamesi.webdev@gmail.com>
2025-12-21 11:26:54 +0330

    change SignUpResponseModel for /signup

--- app/schemas/users_schema.py
+++ app/schemas/users_schema.py
@@ -1,21 +1,14 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
-
 class SignUpModel(BaseModel):
-    id: Optional[int] = None
     username: Optional[str] = None
     email: EmailStr
     password: str
     is_active: Optional[bool] = False
     is_staff: Optional[bool] = False
-
 class SignUpResponseModel(BaseModel):
-    id: int
     username: Optional[str] = None
     email: EmailStr
-    is_active: bool
-    is_staff: bool
-
     class Config:
         orm_mode = True
         schema_extra = {
