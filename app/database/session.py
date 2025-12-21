34fd84e22c0e0afdd4e3e7d3eaddb1464095bc76
sadra khamesi <sadrakhamesi.webdev@gmail.com>
2025-12-21 11:24:21 +0330

    change SignUpResponseModel for /signup

--- app/database/session.py
+++ app/database/session.py
@@ -1,6 +1,14 @@
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database import engine
-Session = sessionmaker(bind=engine,
-                       autocommit=True,
-                       autoflush=False,
-                       expire_on_commit=False)
+import os
+load_dotenv()
+POSTGRES_URI = os.getenv("POSTGRES_URI")
+engine = create_engine(POSTGRES_URI,echo=True)
+Session = sessionmaker(bind=engine,autoflush=False,expire_on_commit=False)
+def get_database():
+    db =Session()
+    try:
+        yield db
+    finally:
+        db.close()
