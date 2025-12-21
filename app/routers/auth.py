f9eb66d0558d35453bd3c76738df8e515dae239d
sadra khamesi <sadrakhamesi.webdev@gmail.com>
2025-12-21 11:29:22 +0330

    add signup service to auth

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends,HTTPException,status
+from app.database.session import get_database
+from app.database.session import Session
+from app.schemas.users_schema import SignUpModel,SignUpResponseModel
+from app.services.user_services import UserGetService,UserCreateService,PasswordHasherService
+auth_router = APIRouter(prefix="/api",tags=["auth_users"])
 
-auth_router = APIRouter(prefix="/api")
 
-@auth_router.get("/auth")
-async def root():
-    return {"message": "its working"}
+@auth_router.post("/signup", response_model=SignUpResponseModel,status_code=status.HTTP_201_CREATED)
+async def signup(user_create: SignUpModel,db:Session = Depends(get_database)):
+    user_service = UserCreateService(db)
+    try:
+        user = user_service.create_user(user_create)
+        return user
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
