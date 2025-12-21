2cafbbbae212ed0ed363a034a4059effdb865716
sadra khamesi <sadrakhamesi.webdev@gmail.com>
2025-12-21 11:32:08 +0330

    add username validation to UserCreateService.create_user

--- app/services/user_services.py
+++ app/services/user_services.py
@@ -3,9 +3,7 @@ from sqlalchemy.orm import Session
 from app.models.users import Users
 from app.schemas.users_schema import SignUpModel
 from passlib.context import  CryptContext
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 class PasswordHasherService:
     def __init__(self, password_hasher: CryptContext):
         self.password_hasher = password_hasher
@@ -37,6 +35,8 @@ class UserCreateService:
     def create_user(self, user_data : SignUpModel ) -> Users:#password should be hashed and not show plain
         if self.get_service.get_user_by_email(user_data.email):
             raise ValueError("This Email already Registered")
+        elif self.get_service.get_user_by_username(user_data.username):
+            raise ValueError("This Username already Registered")
         hashed_password = self.hasher.hash_password(user_data.password)
         db_user = Users(
             username=user_data.username,
