bc129d422ff7d175fdd7a3f7843b3c27a339bbef
sadra khamesi <sadrakhamesi.webdev@gmail.com>
2025-12-22 11:01:50 +0330

    update main: order_router

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.routers.orders  import order_router
 from app.routers.users import user_router
 app = FastAPI(title="Pizza Delivery ElenPizo",summary="Pizza Delivery Web Service")
 app.include_router(auth_router,prefix="/auth")
-app.include_router(order_router)
+app.include_router(order_router,prefix="/orders")
 app.include_router(user_router)
 @app.get("/",tags=["index"])
 async def root():
